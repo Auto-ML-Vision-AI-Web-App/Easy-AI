ae09d3d7f661eadee8843750b667e68a59e0b5cf
Dong-Hun Gang <1031nice@gmail.com>
2021-05-10 00:27:20 +0900

    spring boot project setting

---
+++ api-server/src/main/java/com/eavy/EavyApplication.java
@@ -0,0 +1,13 @@
+package com.eavy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EavyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EavyApplication.class, args);
+    }
+
+}
---
+++ api-server/src/test/java/com/eavy/EavyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eavy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EavyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
