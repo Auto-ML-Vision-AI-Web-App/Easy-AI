f473e4853d00e512b456edd0cedd9a069b6de358
Dong-Hun Gang <1031nice@gmail.com>
2021-05-17 20:45:58 +0900

    file upload handler 추가

--- api-server/src/main/java/com/eavy/Controller.java
+++ api-server/src/main/java/com/eavy/Controller.java
@@ -1,11 +1,26 @@
 package com.eavy;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
 
+    private final ResourceLoader resourceLoader;
+
+    // *******************************
+    /* meaningless method */
     @GetMapping("/")
     public String index() {
         return "eavy";
@@ -15,6 +30,24 @@ public class Controller {
     public AI getModel() {
         return new AI("MLPClassifier", "logistic", "adaptive",
                 50, 0.1, "sgd");
+    }
+    // *******************************
+
+    @PostMapping("/file-upload")
+    public String fileUpload(@RequestParam("file")MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
+        if(!extension.equals("jpg") && !extension.equals("png")) {
+            return "fail";
+        }
+
+        byte[] bytes = file.getBytes();
+        BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(
+                new FileOutputStream("./src/main/resources/images/" + file.getOriginalFilename()));
+        bufferedOutputStream.write(bytes);
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+        return "success";
     }
 
 }
---
+++ api-server/src/test/java/com/eavy/ControllerTest.java
@@ -0,0 +1,53 @@
+package com.eavy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+class ControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @DisplayName("Image file upload success")
+    @Test
+    void fileUploadSuccess() throws Exception {
+        // TODO png에 대해 테스트 되지 않음
+        String originalFilename = "test_file.jpg";
+        MockMultipartFile mockFile = new MockMultipartFile("file", originalFilename, "image/jpeg", getClass().getResourceAsStream("/images/wakeupcat.jpg"));
+
+        mockMvc.perform(multipart("/file-upload").file(mockFile))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string("success"));
+    }
+
+    @DisplayName("Image file upload fail(no image file)")
+    @Test
+    void fileUploadFail() throws Exception {
+        // TODO txt 이외의 다른 형식에 대해 테스트 되지 않음
+        String originalFilename = "test_file.txt";
+        MockMultipartFile mockFile = new MockMultipartFile("file", originalFilename, "text/plain", getClass().getResourceAsStream("/images/test_file.txt"));
+
+        mockMvc.perform(multipart("/file-upload").file(mockFile))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string("fail"));
+    }
+
+}
