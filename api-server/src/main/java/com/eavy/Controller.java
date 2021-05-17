51dde2184c57bdcdd8a855366284ba67bdea845d
h01010 <seoyeonju1198@gmail.com>
2021-05-17 20:18:58 +0900

    add api call function

--- api-server/src/main/java/com/eavy/Controller.java
+++ api-server/src/main/java/com/eavy/Controller.java
@@ -1,8 +1,10 @@
 package com.eavy;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class Controller {
 
@@ -10,11 +12,11 @@ public class Controller {
     public String index() {
         return "eavy";
     }
-
+/*
     @GetMapping("/model")
     public AI getModel() {
         return new AI("MLPClassifier", "logistic", "adaptive",
                 50, 0.1, "sgd");
     }
-
+    */
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+function api_test () {
+  const request = 'http://localhost:8080/'
+  axios.get(request)
+        .then(function(response) {
+            console.log(response.data)
+            console.log("성공");
+        })
+        .catch(function(error) {
+            console.log("실패");
+        })
+};
 
 function App() {
   return (
@@ -7,16 +21,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eavy AI App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={api_test}>API Test</button>
       </header>
     </div>
   );
