ef9c7a68e87e336c581b7b29a273297f487b1feb
h01010 <seoyeonju1198@gmail.com>
2021-06-06 20:04:45 +0900

    ADD model generating function

---
+++ frontend/src/components/Header/HeaderLinks.js
@@ -0,0 +1,83 @@
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="나의 AI"
+          buttonProps={{
+            className: classes.navLink,
+            color: "info",
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="admin/data-uploading" className={classes.dropdownLink}>
+              데이터 업로드
+            </Link>,
+            <Link to="admin/data-checking" className={classes.dropdownLink}>
+              데이터 확인하기
+            </Link>,
+            <Link to="admin/ai-making" className={classes.dropdownLink}>
+              AI 만들기
+            </Link>,
+            <Link to="admin/ai-checking" className={classes.dropdownLink}>
+              AI 결과 확인하기
+            </Link>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/material-kit-react?ref=mkr-navbar"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <CloudDownload className={classes.icons} /> Download
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-twitter"
+          title="Follow us on twitter"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://twitter.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-twitter"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
---
+++ frontend/src/views/Components/AIResult.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
+
+export default function AIResult(props) {
+    const classes = useStyles();
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+    return (
+        <>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={fixedHeightPaper}>
+                        <h4>{props.result_model}</h4>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
