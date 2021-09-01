08eec35fa8a6751d416a8574348a4493157d3bb4
Dong-Hun <1031nice@gmail.com>
2021-09-01 22:08:53 +0900

    [추가] 회원가입 테스트 코드 작성

--- api-server/src/test/java/com/eavy/account/AccountControllerTest.java
+++ api-server/src/test/java/com/eavy/account/AccountControllerTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,19 +17,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private AccountRepository accountRepository;
+    private AccountService accountService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @DisplayName("[로그인 성공] DB에 저장된 정보와 일치하는 경우")
+    private final String TEST_ID = "TESTID";
+    private final String TEST_PASSWORD = "TESTPASSWORD";
+
+    @DisplayName("[로그인 성공]")
     @Test
     void signInSuccess() throws Exception {
-        Account account = new Account("donghun", "1031");
-        accountRepository.save(account);
+        Account account = new Account(TEST_ID, TEST_PASSWORD);
+        accountService.signUp(account);
 
+        // TODO save한 객체를 그대로 쓰는게 best prectice인걸로 알고 있는데, 그럼 encoded password 객체를 받아서 사용하는게 맞는지?
         mockMvc.perform(post("/signin")
             .param("userId", account.getUserId())
             .param("password", account.getPassword()))
@@ -33,14 +44,37 @@ class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("[로그인 실패] DB에 저장된 정보와 일치하지 않는 경우")
+    @DisplayName("[로그인 실패] 정보 불일치")
     @Test
     void signInFailure() throws Exception {
         mockMvc.perform(post("/signin")
-                .param("userId", "SUPERAWESOMEID")
-                .param("password", "SUPERSECUREPASSWORD"))
+                .param("userId", TEST_ID)
+                .param("password", TEST_PASSWORD))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("[회원가입 성공]")
+    @Test
+    void signUpSuccess() throws Exception {
+        mockMvc.perform(post("/signup")
+                        .param("userId", TEST_ID)
+                        .param("password", TEST_PASSWORD))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("[회원가입 실패] 중복된 아이디")
+    @Test
+    void signUpFailure() throws Exception {
+        Account account = new Account(TEST_ID, TEST_PASSWORD);
+        accountService.signUp(account);
+
+        mockMvc.perform(post("/signup")
+                        .param("userId", account.getUserId())
+                        .param("password", account.getPassword()))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
