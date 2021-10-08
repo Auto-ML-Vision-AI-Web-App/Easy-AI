e87ad6ca7228897e1641cab5056717db69694ee2
h01010 <seoyeonju1198@gmail.com>
2021-10-08 19:56:25 +0900

    Change Components.js to Home.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.10.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
+import Home from "views/Components/Home.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
@@ -23,7 +23,7 @@ ReactDOM.render(
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/admin" component={Admin} />
-      <Route path="/" component={Components} />
+      <Route path="/" component={Home} />
     </Switch>
   </Router>,
   document.getElementById("root")
