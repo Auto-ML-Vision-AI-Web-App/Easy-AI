fb28c993d4f4c48c58fa28d941ac183ad1a3828b
Dong-Hun <1031nice@gmail.com>
2021-10-09 15:13:11 +0900

    [추가] 프로젝트 생성 API 테스트

---
+++ api-server/src/main/java/com/eavy/project/ProjectController.java
@@ -0,0 +1,7 @@
+package com.eavy.project;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProjectController {
+}
---
+++ api-server/src/test/java/com/eavy/project/ProjectControllerTest.java
@@ -0,0 +1,33 @@
+package com.eavy.project;
+
+import com.eavy.account.Account;
+import com.eavy.common.ControllerTest;
+import com.eavy.token.TokenManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ProjectControllerTest extends ControllerTest {
+
+    @DisplayName("프로젝트 생성")
+    @Test
+    void createProject() throws Exception {
+        Account account = accountService.signUp(new Account(TEST_ID, TEST_PASSWORD));
+        String accessToken = TokenManager.generateAccessToken(TEST_ID);
+        String projectName = "prj1";
+        mockMvc.perform(post("/project")
+                .param("projectName", projectName)
+                .header("Authorization", "Bearer " + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        assertThat(account.getProjects().get(0).getName()).isEqualTo(projectName);
+    }
+
+}
