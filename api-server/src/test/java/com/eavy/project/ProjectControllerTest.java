ba9a2cdbcb4065380ff11d147abc49709242f930
Dong-Hun <1031nice@gmail.com>
2021-11-25 14:29:07 +0900

    [추가] 프로젝트 정보 업데이트 API 테스트

--- api-server/src/test/java/com/eavy/project/ProjectControllerTest.java
+++ api-server/src/test/java/com/eavy/project/ProjectControllerTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,6 +20,34 @@ class ProjectControllerTest extends ControllerTest {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    @DisplayName("프로젝트 업데이트")
+    @Test
+    void updateProject() throws Exception {
+        Account account = accountService.signUp(new Account(TEST_ID, TEST_PASSWORD));
+        Project project = new Project("prj1");
+        account.addProject(project);
+        Project savedProject = projectRepository.save(project);
+        String accessToken = TokenManager.generateAccessToken(TEST_ID);
+
+        double accuracy = 0.82;
+        double loss = 0.26;
+        List<String> classes = List.of("c1, c2");
+
+        mockMvc.perform(patch("/projects")
+                        .param("projectName", project.getName())
+                        .param("accuracy", "0.82")
+                        .param("classes", classes.get(0), classes.get(1))
+                        .param("loss", "0.26")
+                        .header("Authorization", "Bearer " + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(project.getName()));
+
+        assertThat(savedProject.getAccuracy()).isEqualTo(accuracy);
+        assertThat(savedProject.getLoss()).isEqualTo(loss);
+        assertThat(savedProject.getClasses()).isEqualTo(classes);
+    }
 
     @DisplayName("프로젝트 생성")
     @Test
